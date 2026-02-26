2cb48015043cebc206d6c02d798154bb4e954b27
kishalay_0021 <noobop721649@gmail.com>
2026-02-26 18:00:11 +0530

    auth fix
    Enhance JwtAuthGuard to include custom authorization logic for user verification.

--- src/auth/guards/Auth.guard.ts
+++ src/auth/guards/Auth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException, ExecutionContext } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class JwtAuthGuard extends AuthGuard('jwt') {
+      canActivate(context: ExecutionContext) {
+        return super.canActivate(context);
+    }
+
+    handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+        if (err || !user) {
+            throw new UnauthorizedException('Please Login First!');
+        }
+        
+        if (!user.isVeryfied) {
+            throw new UnauthorizedException('Please verify your account');
+        }
+        
+        return user;
+    }
+}
